0526a62b18992a28e6bec336428d07a84722c0be
DiZhengShi <trolljoe42069@gmail.com>
2025-03-12 18:17:10 -0500

    vale

--- EcoPage/db/data.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('usuarios.db')
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM usuarios")
-datos = cursor.fetchall()
-conn.close()
-
-print("Datos en la base de datos:", datos)
